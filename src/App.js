12a4e7b929d2021176d6249fe73de914afd366f4
rushikeshkamurthi <k.hrushikesh007@gmail.com>
2023-03-13 19:40:40 +0530

    Added  routes to  the project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import TextForm from './components/textform';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import About from './components/about';
 function App() {
   return (
     <><Navbar title = "Simple React Props ans State Project" />
     <div className='container'>
-    <TextForm/>
-    
+    <Router>
+    <Routes>
+    <Route exact path="/" element={<TextForm></TextForm>} />
+    <Route exact path="/about" element={<About></About>} />
+        </Routes>
+    </Router>
     </div>
     <div className='container '></div>
     <Footer></Footer> 
